c81bacc9b7d08cd186a39975ccb7b5f828e83f34
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-15 14:26:38 -0300

    Projeto criado

---
+++ RequestTest1/RequestTest1/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  RequestTest1
+//
+//  Created by Gilmar Amaro on 15/03/23.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
